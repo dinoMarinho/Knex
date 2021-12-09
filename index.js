4a74276a1c2c9fce4bbe29b6927132858cd6096d
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2021-12-08 21:14:15 -0300

    Aprendendo selects e wheres

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dados = [
    preco: 30.78},
 ]
 
+
 // Inserindo dados
 /*
 database.insert(dados).into('games').then(data => {
@@ -15,3 +16,52 @@ database.insert(dados).into('games').then(data => {
     console.dir(err)
 });
 */
+
+
+// SELECT normal
+/* database.select(["nome","preco"]).table("games").then(data => {
+    console.dir(data);
+}).catch(erro => {
+    console.dir(erro);
+}); */
+
+
+//NESTED QUERIES
+/*
+database.insert({nome: "Mists of noyah", preco: 25})
+    .into("games")
+.then(data => {
+    database.select(["id","preco"])
+        .table("games")
+    .then(data => {
+        console.dir(data);
+    }).catch(erro => {
+        console.dir(erro);
+    });
+}).catch(erro => {
+    console.dir(erro);
+}); */
+
+
+// WHERE
+/*
+database.select(["id", "nome"])
+    .where({id: 3})
+    // .where('columnName', 'like', '%rowlikeme%')
+    .orWhere({id: 4})
+    // .whereRaw('?? = ??', ['a.column_1', 'b.column_2'])
+    .table('games')
+.then(data => {
+    console.dir(data);
+}).catch(erro => {
+    console.dir(erro);
+}); */
+
+
+//RAW
+/*
+database.raw("SELECT * FROM games").then(data => {
+    console.log(data);
+}).catch(erro => {
+    console.log(erro);
+}); */
